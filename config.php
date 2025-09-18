1f90636c229e73c3f7e6372cd3935f26658da51b
PePeralta <pedroafonsoperalta@gmail.com>
2025-09-18 08:35:31 +0100

    Create config.php

---
+++ config.php
@@ -0,0 +1,4 @@
+<?php 
+    $SUPABASE_URL = "https://PhpBases.supabase.co";
+    $SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtnbnVmanhtdnZ0Yml6Z3phZW9zIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgxNzg1MTQsImV4cCI6MjA3Mzc1NDUxNH0.NaTo5_7koRzyqVjCCCfoLnciqYlE4tusCBcSez0PcDo";
+?>
